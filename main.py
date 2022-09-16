e7de6a558112357da30bb477dfbbe03a6969cc02
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-09-16 16:06:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import siscom.modulador
 import numpy as np
 
 arquivo = 'C:/Users/Clayton J A Silva/PycharmProjects/simuladorSisCom/siscom/voz1_clayton.wav'
-a, b = siscom.codecvoz.captura(arquivo,dominioVoz='f')
+a, b = siscom.codecvoz.captura(arquivo,dominioVoz='t')
+#t, x = siscom.codecvoz.amostragem(b, a, 0.03)
+#t = np.array([i * a for i in range(len(b))])
 
-# t, x = siscom.modulador.sinalFM(sinal,t,10000,delta=10,dominioFM='f')
+f, X = siscom.modulador.sinalAM(b,a,100000,dominioAM='f')
 
-plt.plot(a,b)
+plt.plot(f,X)
 plt.show()
