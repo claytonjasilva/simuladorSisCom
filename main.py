e5c56d72ee3809518b242868d0305ff4239f1d63
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-09-17 10:53:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import matplotlib.pyplot as plt
 import siscom.sinais
 import siscom.codecvoz
 import siscom.modulador
+import siscom.canal
 import numpy as np
 
 # arquivo = 'C:/Users/Clayton J A Silva/PycharmProjects/simuladorSisCom/siscom/voz1_clayton.wav'
 # a, b = siscom.codecvoz.captura(arquivo,dominioVoz='t')
 
-t, x = siscom.sinais.quadrada(10000,0.001, 50, amplitude=2.0, delay=2.8, dominio='f')
+r = siscom.sinais.senoide(10000,0.001,56)
+h = siscom.sinais.impulso(10000,0.001,amplitude = 2)
 
-plt.plot(t,x)
-plt.show()
+c = siscom.canal.capacidade(r[1], h[1], 100)
+print(c)
+#plt.plot(t,x)
+#plt.show()
