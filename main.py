53b8cdc0e7a26d311da0e9863ab6c08abe516158
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-09-18 11:19:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,10 @@ import siscom.modulador
 import siscom.canal
 import numpy as np
 
-# arquivo = 'C:/Users/Clayton J A Silva/PycharmProjects/simuladorSisCom/siscom/voz1_clayton.wav'
-# a, b = siscom.codecvoz.captura(arquivo,dominioVoz='t')
+arquivo = 'C:/Users/Clayton J A Silva/PycharmProjects/simuladorSisCom/siscom/voz1_clayton.wav'
+b = siscom.codecvoz.captura(arquivo)
+# tempo = np.array([i * b[0] for i in range(len(b[1]))])
+t,x = siscom.codecvoz.aaliasing(b[1],b[0],2000, dominio='f')
 
-r = siscom.sinais.senoide(10000,0.001,56)
-h = siscom.sinais.impulso(10000,0.001,amplitude = 2)
-
-c = siscom.canal.capacidade(r[1], h[1], 100)
-print(c)
-#plt.plot(t,x)
-#plt.show()
+plt.plot(t,x)
+plt.show()
