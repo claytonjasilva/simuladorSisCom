fdaae568736453b17cbff7acce74d1fa9d3e00a4
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-09-18 11:20:07 -0300

    Update codecvoz.py

--- siscom/codecvoz.py
+++ siscom/codecvoz.py
@@ -7,58 +7,63 @@ import numpy as np
 import siscom.sinais
 
 
-def captura(arquivo, dominioVoz='t'):
+def captura(arquivo, dominio='t'):
     output = wavfile.read(arquivo)  # O arquivo .wav deve ser um canal - mono
     Ta = 1 / output[0]  # Ta - período de amostragem
     x = np.array(output[1], dtype=float)  # array relativo ao arquivo de voz
     Xf = np.fft.fft(x)
     f = np.fft.fftfreq(len(Xf), Ta)
-    if dominioVoz=='t':
+    if dominio=='t':
         return Ta, x
     else:
         return f, Xf
 
 
-def amostragem(sinal, Tasinal, Ta):
-    Ns = len(sinal)
-    #N = int(Ta / (Tasinal / (len(sinal) - 1)))  # Número de amostras do sinal em intervalo Ta
+def amostragem(sinal, Tasinal, Ta, dominio='t'):
     N = int(Ta/Tasinal)
-    delta = np.array([0.0] * Ns)
-    for i in range(Ns):
+    x = [0.0] * len(sinal)
+    for i in range(len(sinal)):
         if (i % N) == 0:
-            t, x = siscom.sinais.impulso(Ns, Ta, delay=(i * Ta))
-            delta += np.array(x)  # delta(Ta)
-    x = np.multiply(sinal, delta)  # Amostragem do sinal
-    t = np.array([i * (Tasinal / (len(sinal) - 1)) for i in range(Ns)])
-    return t, x
+            x[i] = sinal[i]
+    t = np.array([i * Tasinal for i in range(len(sinal))])
+    Xf = np.fft.fft(x)
+    f = np.fft.fftfreq(len(Xf), Tasinal)
+    if dominio=='t':
+        return t, x
+    else:
+        return f, Xf
 
 
-def quantizado(sinal, Ta, L):
+def quantizado(sinal, Ta, L, dominio='t'):
     vmax = np.amax(sinal)
     vmin = np.amin(sinal)
     degrau = (vmax - vmin) / (L - 1)
-    nivel = np.array([vmin + i * degrau for i in range(L)])  # Níveis da quantização
+    nivel = [vmin + i * degrau for i in range(L)]
     x = np.array([0.0] * len(sinal))
     for i in range(len(sinal)):  # Quantizador linear - menor distância
-        x[i] = nivel[0]
-        min = abs(sinal[i] - nivel[0])
-        for j in range(1, L):
-            if abs(sinal[i] - nivel[j]) < min:
-                min = abs(sinal[i] - nivel[j])
-                x[i] = nivel[j]
+        decisao = [abs(sinal[i] - x[j]) for j in range(L)]
+        menor = min(decisao)
+        ind = decisao.index(menor)
+        x[i] = nivel[ind]
     t = np.array([i * Ta for i in range(len(sinal))])
+    Xf = np.fft.fft(x)
+    f = np.fft.fftfreq(len(Xf), Ta)
+    if dominio=='t':
+        return t, x
+    else:
+        return f, Xf
 
-    return t, x
 
-
-def aaliasing(sinal, Ta, fcorte):
-    Sf = np.absolute(np.fft.fft(sinal))
-    f = np.fft.fftfreq(len(sinal), Ta)
-    filtro = np.array([0.0] * len(Sf))
+def aaliasing(sinal, Ta, fcorte, dominio='t'):
+    Sf = np.fft.fft(sinal)
+    Xf = np.array([0.0] * len(Sf))
+    f = np.fft.fftfreq(len(Sf), Ta)
     for i in range(len(Sf)):
-        if fcorte >= np.absolute(f[i]):
-            filtro[i] = 1.0  # filtro passa-baixas
-    Xf = np.multiply(Sf, filtro)
+        if fcorte >= abs(f[i]):
+            Xf[i] = Sf[i]  # filtro passa-baixas
     x = np.fft.ifft(Xf)
     t = np.array([i * Ta for i in range(len(x))])
-    return t, x
+    if dominio=='t':
+        return t, x
+    else:
+        return f, Xf
