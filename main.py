48a71b6a38054b621680b1700a714254d295e49b
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-09-19 13:42:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ import siscom.modulador
 import siscom.canal
 import numpy as np
 
-arquivo = 'C:/Users/Clayton J A Silva/PycharmProjects/simuladorSisCom/siscom/voz1_clayton.wav'
-b = siscom.codecvoz.captura(arquivo)
+#arquivo = 'C:/Users/Clayton J A Silva/PycharmProjects/simuladorSisCom/siscom/voz1_clayton.wav'
+#b = siscom.codecvoz.captura(arquivo)
 # tempo = np.array([i * b[0] for i in range(len(b[1]))])
-t,x = siscom.codecvoz.aaliasing(b[1],b[0],2000, dominio='f')
+#mod = np.array([1.] * 30000)
+#t,x = siscom.modulador.sinalAM(b[1],2300000)
+#print(1/2300000)
 
+t, x = siscom.sinais.porta(50,20000,0.01,delay=2)
 plt.plot(t,x)
 plt.show()
