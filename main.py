c8d732954f9b8efb63461330377feed307e8ca82
Daniel Carlier <daniel.carlier@hotmail.com>
2022-10-18 19:05:44 -0300

    [FEAT]Add modulators

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 
 
 import matplotlib.pyplot as plt
-import siscom.sinais
-import siscom.codecvoz
+# import siscom.sinais
+# import siscom.codecvoz
 import siscom.modulador
-import siscom.canal
-import numpy as np
+# import siscom.canal
 
 #arquivo = 'C:/Users/Clayton J A Silva/PycharmProjects/simuladorSisCom/siscom/voz1_clayton.wav'
 #b = siscom.codecvoz.captura(arquivo)
@@ -15,6 +14,32 @@ import numpy as np
 #t,x = siscom.modulador.sinalAM(b[1],2300000)
 #print(1/2300000)
 
-t, x = siscom.sinais.porta(50,20000,0.01,delay=2)
-plt.plot(t,x)
+# simbolos = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]
+cad_caracteres = input('Digite um frame de uma mensagem:\n')
+select_mode = input('Digite para escolher: 1 (3 bits por simbolo) ou 2 (4 bits por simbolo)\n')
+simbolos = siscom.modulador.geraSimbolos(cad_caracteres, select_mode)
+fc = 10000
+figure, axis = plt.subplots(2)
+
+# 8-PSM
+t, x = siscom.modulador.geraSinalPAM(simbolos, fc)
+f, Xf = siscom.modulador.geraSinalPAM(simbolos, fc, 'f')
+axis[0].set_title("8-PSM - Tempo")
+axis[1].set_title("8-PSM - Frequência")
+
+# 8-PSK
+# t, x = siscom.modulador.geraSinalPSK(simbolos, fc)
+# f, Xf = siscom.modulador.geraSinalPAM(simbolos, fc, 'f')
+# axis[0].set_title("8-PSK - Tempo")
+# axis[1].set_title("8-PSK - Frequência")
+
+# 16-QAM
+# t, x = siscom.modulador.geraSinalQAM(simbolos, fc)
+# f, Xf = siscom.modulador.geraSinalQAM(simbolos, fc, 'f')
+# axis[0].set_title("16-QAM - Tempo")
+# axis[1].set_title("16-QAM - Frequência")
+
+
+axis[0].plot(t,x)
+axis[1].plot(f,Xf)
 plt.show()
--- siscom/modulador.py
+++ siscom/modulador.py
@@ -45,3 +45,117 @@ def sinalFM(modulante, Ta, fc, delta=1, A=1, dominioFM='t'):
         return t, x
     else:
         return f, Xf
+
+def geraSimbolos(cad_caracteres, select_mode):
+    lista_binarios=[]
+    lista_de_simbolos = []
+
+    if select_mode == "1":
+        for carac in cad_caracteres:
+            ascii_palavra = ord(carac)
+            string_binario = bin(ascii_palavra)
+            string_sem_0b = string_binario.replace('0b','')
+            string_completa_8_bits = string_sem_0b.zfill(8)
+            lista_binarios.append(string_completa_8_bits)
+
+            for palavra_binaria in lista_binarios:
+                bits_1 = palavra_binaria[0:3]
+                bits_2 = palavra_binaria[3:6]
+                bits_3 = palavra_binaria[6:]
+
+            lista_de_simbolos.append(int(bits_1, 2))
+            lista_de_simbolos.append(int(bits_2, 2))
+            lista_de_simbolos.append(int(bits_3, 2))
+
+        return lista_de_simbolos
+
+    elif select_mode == "2":
+        for carac in cad_caracteres:
+            ascii_palavra = ord(carac)
+            string_binario = bin(ascii_palavra)
+            string_sem_0b = string_binario.replace('0b','')
+            string_completa_8_bits = string_sem_0b.zfill(8)
+            lista_binarios.append(string_completa_8_bits)
+
+            for palavra_binaria in lista_binarios:
+                bits_1 = palavra_binaria[0:4]
+                bits_2 = palavra_binaria[4:]
+
+            lista_de_simbolos.append(int(bits_1, 2))
+            lista_de_simbolos.append(int(bits_2, 2))
+
+        return lista_de_simbolos
+
+    else:
+        print('erro! Código inválido!')
+
+def geraSinalPAM(simbolosPAM, fc, dominio='t'):
+    s = []
+    d = 1
+    M = 8
+    gt = 1
+    Tc = 1 / fc
+    Fa = 50 * fc
+    Ta = 1 / Fa
+
+    t = [i * Ta for i in range(len(simbolosPAM) * int(Tc / Ta))]
+    for simbolo in simbolosPAM:
+        Am = ((2 * simbolo) - M) * d
+        for i in range(int(Tc / Ta)):
+            s.append(Am * gt * np.cos(2 * math.pi * fc * t[i]))
+
+    Xf = np.absolute(np.fft.fft(s))
+    f = np.fft.fftfreq(len(Xf), Ta)
+
+    if dominio == 't':
+        return t, s
+
+    return f, Xf
+
+def geraSinalPSK(simbolosPSK, fc, dominio='t'):
+    s = []
+    M = 8
+    gt = 1
+    Tc = 1 / fc
+    Fa = 50 * fc
+    Ta = 1 / Fa
+
+    t = [i * Ta for i in range(len(simbolosPSK) * int(Tc / Ta))]
+    for simbolo in simbolosPSK:
+        for i in range(int(Tc / Ta)):
+            op = (2 * math.pi * fc * t[i]) + ((2 * math.pi / M) * (simbolo - 1))
+            s.append(gt * np.cos(op))
+
+    Xf = np.absolute(np.fft.fft(s))
+    f = np.fft.fftfreq(len(Xf), Ta)
+
+    if dominio == 't':
+        return t, s
+
+    return f, Xf
+
+def geraSinalQAM(simbolosQAM, fc, dominio='t'):
+    s = []
+    M = 16
+    N = 16
+    d = 1
+    gt = 1
+    Tc = 1 / fc
+    Fa = 50 * fc
+    Ta = 1 / Fa
+
+    t = [i * Ta for i in range(len(simbolosQAM) * int(Tc / Ta))]
+    for simbolo in simbolosQAM:
+        Am = (2 * simbolo - 1 - M) * d
+        phi = (2 * math.pi / N) * (simbolo - 1)
+        for i in range(int(Tc / Ta)):
+            op = 2 * math.pi * fc * t[i] + phi
+            s.append(Am * gt * np.cos(op))
+
+    Xf = np.absolute(np.fft.fft(s))
+    f = np.fft.fftfreq(len(Xf), Ta)
+
+    if dominio == 't':
+        return t, s
+
+    return f, Xf
