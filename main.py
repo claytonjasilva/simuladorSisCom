84004a20285f92298e649ff0548f58c22321b0c8
Daniel Carlier <daniel.carlier@hotmail.com>
2022-10-21 10:38:20 -0300

    [FEAT]Add grupoA changes

--- main.py
+++ main.py
@@ -10,27 +10,26 @@ simbolos = siscom.modulador.geraSimbolos(cad_caracteres, select_mode)
 fc = 10000
 figure, axis = plt.subplots(2)
 
-match choose_option:
-    case "1":
-        # 8-PAM
-        t, x = siscom.modulador.geraSinalPAM(simbolos, fc)
-        f, Xf = siscom.modulador.geraSinalPAM(simbolos, fc, 'f')
-        axis[0].set_title("8-PAM - Tempo")
-        axis[1].set_title("8-PAM - Frequência")
+if choose_option == "1":
+    # 8-PAM
+    t, x = siscom.modulador.geraSinalPAM(simbolos, fc)
+    f, Xf = siscom.modulador.geraSinalPAM(simbolos, fc, 'f')
+    axis[0].set_title("8-PAM - Tempo")
+    axis[1].set_title("8-PAM - Frequência")
 
-    case "2":
-        # 8-PSK
-        t, x = siscom.modulador.geraSinalPSK(simbolos, fc)
-        f, Xf = siscom.modulador.geraSinalPAM(simbolos, fc, 'f')
-        axis[0].set_title("8-PSK - Tempo")
-        axis[1].set_title("8-PSK - Frequência")
+elif choose_option == "2":
+    # 8-PSK
+    t, x = siscom.modulador.geraSinalPSK(simbolos, fc)
+    f, Xf = siscom.modulador.geraSinalPAM(simbolos, fc, 'f')
+    axis[0].set_title("8-PSK - Tempo")
+    axis[1].set_title("8-PSK - Frequência")
 
-    case "3":
-        # 16-QAM
-        t, x = siscom.modulador.geraSinalQAM(simbolos, fc)
-        f, Xf = siscom.modulador.geraSinalQAM(simbolos, fc, 'f')
-        axis[0].set_title("16-QAM - Tempo")
-        axis[1].set_title("16-QAM - Frequência")
+elif choose_option == "3":
+    # 16-QAM
+    t, x = siscom.modulador.geraSinalQAM(simbolos, fc)
+    f, Xf = siscom.modulador.geraSinalQAM(simbolos, fc, 'f')
+    axis[0].set_title("16-QAM - Tempo")
+    axis[1].set_title("16-QAM - Frequência")
 
 axis[0].plot(t,x)
 axis[1].plot(f,Xf)
@@ -40,13 +39,15 @@ if choose_option == "2" or choose_option == "3":
     generateConstelation = input('Digite para escolher: 1 - Gera constelacao ou 2 - Não gera constelacao\n')
     if generateConstelation == "1":
         Ta = 1 / (50*fc)
-        tR, xR = siscom.canal.sinRuido(x, Ta, 0.5)
+        tR, xR = siscom.canal.sinRuido(x, Ta, 100)
         magnitude, phase = siscom.modulador.geraConstelacao(xR)
-
-        constFigure, constAxis = plt.subplots(2)
-        constAxis[0].plot(tR,xR)
-        constAxis[0].set_title("Ruido")
-        constAxis[1].scatter(magnitude, phase)
-        constAxis[1].set_title("Constelação")
+        fig = plt.figure()
+        ax1 = plt.subplot(121)
+        ax2 = plt.subplot(122, projection='polar')
+        ax1.set_title("Sinal com ruído")
+        ax1.plot(tR, xR)
+        ax2.set_title("Constelação")
+        ax2.scatter(magnitude, phase)
+        ax2.set_rorigin(0)
         plt.grid(True)
         plt.show()
--- siscom/grupoA/grupo.md
+++ siscom/grupoA/grupo.md
@@ -4,3 +4,6 @@
 1. Phillipe
 2. Gustavo
 3. Daniel
+
+Professor, por conta de termos feito a edição do código em cima do original do simulador, acredito que é melhor entregar através de um branch alternativo e não de uma pasta dentro da branch main para evitar confusões e conflitos de versão. O código editado pelo grupoA está disponível aqui:
+
